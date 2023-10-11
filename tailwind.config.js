bdd4e879cd7e5c83603166ac32d9b776a5291859
Richard Swinkels <github@richardswinkels.nl>
2023-10-11 14:28:09 +0200

    Configure tailwind colors, add inter font and heroicons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: [
     "./components/**/*.{js,vue,ts}",
@@ -8,8 +11,33 @@ export default {
     "./app.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: colors.white,
+        secondary: {
+          100: "#8cb9ff",
+          200: "#81b2ff",
+          300: "#74aaff",
+          400: "#66a2ff",
+          500: "#5799ff",
+          600: "#468fff",
+          700: "#3384ff",
+          800: "#1f78ff",
+          900: "#0066ff",
+        },
+        accent: {
+          700: "#17293c",
+          800: "#111e2c",
+          900: "#0b141d",
+        },
+      },
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
   },
-  plugins: [],
-}
-
+};
