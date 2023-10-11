913590282ef6bb12e2b3e4d959474c335db2962f
Richard Swinkels <github@richardswinkels.nl>
2023-10-11 17:26:03 +0200

    lint files

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,4 @@
 <template>
-    <div>
-        <h1>Hello World</h1>
-    </div>
+  <HeroSection />
+  <AboutSection />
 </template>
-
-<script setup lang="ts">
-
-</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,43 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require("tailwindcss/colors");
-const defaultTheme = require("tailwindcss/defaultTheme");
+import colors from 'tailwindcss/colors';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue',
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
+        sans: ['Inter Variable', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: colors.white,
         secondary: {
-          100: "#8cb9ff",
-          200: "#81b2ff",
-          300: "#74aaff",
-          400: "#66a2ff",
-          500: "#5799ff",
-          600: "#468fff",
-          700: "#3384ff",
-          800: "#1f78ff",
-          900: "#0066ff",
+          100: '#8cb9ff',
+          200: '#81b2ff',
+          300: '#74aaff',
+          400: '#66a2ff',
+          500: '#5799ff',
+          600: '#468fff',
+          700: '#3384ff',
+          800: '#1f78ff',
+          900: '#0066ff',
         },
         accent: {
-          700: "#17293c",
-          800: "#111e2c",
-          900: "#0b141d",
+          700: '#17293c',
+          800: '#111e2c',
+          900: '#0b141d',
         },
       },
     },
     container: {
       center: true,
-      padding: "1rem",
+      padding: '1rem',
     },
   },
 };
