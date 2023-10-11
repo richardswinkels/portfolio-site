3fe8b5fa9735e5d2ae6fdfea20244ad415737453
Richard Swinkels <github@richardswinkels.nl>
2023-10-11 17:27:05 +0200

    Move assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/main.scss'],
+  css: ['~/assets/css/main.scss'],
   postcss: {
     plugins: {
       tailwindcss: {},
