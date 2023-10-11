33ed093c25e561c8fbe930d89bd45885957ef1dd
Richard Swinkels <github@richardswinkels.nl>
2023-10-11 17:22:23 +0200

    Install ESlint, Prettier and tailwind plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ["~/assets/main.scss"],
+  css: ['~/assets/main.scss'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-  modules: [    
-    '@nuxt/content'  
-  ],
-})
+  modules: ['@nuxt/content'],
+});
