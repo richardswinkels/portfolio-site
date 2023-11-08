5a6e596494c9b1df818639391eab48d4610c9fc9
Richard Swinkels <github@richardswinkels.nl>
2023-11-08 23:17:44 +0100

    Add seo

--- app.vue
+++ app.vue
@@ -5,3 +5,16 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+useSeoMeta({
+  title: 'Richard Swinkels - Software Developer',
+  ogTitle: 'Richard Swinkels - Software Developer',
+  description:
+    'Portfolio van Richard Swinkels, Software Developer uit regio Eindhoven.',
+  ogDescription:
+    'Portfolio van Richard Swinkels, Software Developer uit regio Eindhoven.',
+  ogImage: 'https://richardswinkels.nl/img/photo.jpg',
+  twitterCard: 'summary_large_image',
+});
+</script>
